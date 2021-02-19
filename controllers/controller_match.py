3fdc2e7898f97cfa650022d8e37fc65a42e4639e
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-02-19 18:08:13 +0200

    Match results added in Controller and Viewer

--- controllers/controller_match.py
+++ controllers/controller_match.py
@@ -18,7 +18,7 @@ class ControllerMatch:
 		self.model = model_match
 		self.view = view_match
 
-	def print_match(first_player, second_player):
+	def start_match(first_player, second_player):
 		first_player_name = first_player.family_name
 		second_player_name = second_player.family_name
 		return ViewMatch.start_match(first_player_name, second_player_name)
@@ -35,19 +35,24 @@ class ControllerMatch:
 	    if result in "wW":
 	        result = ModelMatch.first_player_wins(match)
 	        first_player.score += 1
-	        return ViewMatch.print_first_player_wins_and_score(f_p, s_p)
+	        match = ModelMatch(f_p, 1, s_p, 0) 
+	        ViewMatch.print_first_player_wins_and_score(f_p, s_p)
 
 	    elif result in "lL":
 	        result = ModelMatch.second_player_wins(match)
 	        second_player.score += 1
-	        return ViewMatch.print_first_player_wins_and_score(s_p, f_p)
+	        match = ModelMatch(f_p, 0, s_p, 1) 
+	        ViewMatch.print_first_player_wins_and_score(s_p, f_p)
 
 	    elif result in "dD":
 	        result = ModelMatch.draw(match)
 	        first_player.score += 0.5
 	        second_player.score += 0.5
-	        return ViewMatch.print_draw(f_p, s_p)    
+	        match = ModelMatch(f_p, 0.5, s_p, 0.5) 
+	        ViewMatch.print_draw(f_p, s_p)
 
+	    return match
+	    
 if __name__ == "__main__":
 
 	for _ in range(1,3):
@@ -56,7 +61,7 @@ if __name__ == "__main__":
 	first_player = ModelPlayer.tournament_players[0]
 	second_player = ModelPlayer.tournament_players[1]
 
-	ControllerMatch.print_match(first_player, second_player)
+	ControllerMatch.start_match(first_player, second_player)
 
 	ControllerMatch.print_winner_and_score(first_player, second_player)
 
--- views/view_match.py
+++ views/view_match.py
@@ -1,34 +1,34 @@
 class ViewMatch():
 
-    line = (60*"-")
-    match_result = (" MATCH RESULT ").center(60,"-")
+    line = (100*"-")
+    match_result = (" MATCH RESULT ").center(100,"-")
 
     @staticmethod
     def start_match(first_player, second_player):
-        next_match = (" NEXT MATCH ").center(60,"-")
+        next_match = (" NEXT MATCH ").center(100,"-")
         print(next_match)
-        print(f"{first_player} vs {second_player}".center(60, ' '))
+        print(f"{first_player} vs {second_player}".center(100, ' '))
         print(ViewMatch.line)
 
     @staticmethod
     def get_match_result(first_player):
         result = input(f"Enter result for {first_player} - W (wins), L (looses), D (draw): ")
         while result not in ("wWlLdD"):
-            print(f"Enter result for {first_player} - W (wins), L (looses), D (draw): ")
+            result = input(f"Enter result for {first_player} - W (wins), L (looses), D (draw): ")            
             continue
         return result
 
     @staticmethod
     def print_first_player_wins_and_score(first_player, second_player):
         print("\n" + ViewMatch.match_result)
-        print(f"{first_player} WINS".center(60, ' '))
-        print(f"{first_player}:1 - {second_player}:0".center(60, ' '))
+        print(f"{first_player} WINS".center(100, ' '))
+        print(f"{first_player}:1 - {second_player}:0".center(100, ' '))
         print(ViewMatch.line)
 
 
     @staticmethod
     def print_draw(first_player, second_player):
         print(ViewMatch.match_result)
-        print("DRAW".center(60, ' '))
-        print(f"{first_player}:0.5 - {second_player}:0.5".center(60, ' '))
+        print("DRAW".center(100, ' '))
+        print(f"{first_player}:0.5 - {second_player}:0.5".center(100, ' '))
         print(ViewMatch.line)
