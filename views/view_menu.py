ce64542b4e69361c762565db7afe36a6b8d7f761
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-03-02 16:19:39 +0200

    PEP8 + docstrings completed

--- views/view_menu.py
+++ views/view_menu.py
@@ -8,42 +8,124 @@ class ViewMenu:
     @staticmethod
     def print_welcome_page():
 
+        """A static method to print the welcome page."""
+
         welcome_message = pyfiglet.figlet_format("CHESS SCORES MANAGER")
         print(welcome_message)
         print(ViewMenu.line)
         print("AUTHOR : ALEXANDRE MERANCIENNE")
         print(ViewMenu.line)
-        print("\nCHOOSE OPTION NUMBER: \n")
+        print("\nCHOOSE OPTION NUMBER:\n")
 
     @staticmethod
     def return_first_choice_welcome_page():
+
+        """A static method to return the first choice on the welcome page."""
+
         option_choice = input(("Generate Tournament [1]"
-                              + "    Search Player [2]"
-                              + "    Search Tournament [3]"
+                              + "    Players Database [2]"
+                              + "    Tournaments Database [3]"
                               + "    Quit Program [4]\n\n").center(80))
         while str(option_choice) not in "1234":
-            print("Choose a number between 1 and 4.")
+            print("Choose a number between 1 and 4")
             option_choice = input(("Generate Tournament [1]"
-                                  + "    Search Players [2]"
-                                  + "    Search Tournament [3]"
+                                  + "    Players Database [2]"
+                                  + "    Tournaments Database [3]"
                                   + "    Quit Program [4]\n\n").center(80))
             continue
         return option_choice
 
     @staticmethod
+    def print_first_option():
+
+        """A static method to announce the creation of a new tournament."""
+
+        start_tournament = pyfiglet.figlet_format("NEW TOURNAMENT",
+                                                  font="digital")
+        print(start_tournament)
+
+    @staticmethod
+    def print_player_search_options():
+
+        """A static method to print the search options for a player."""
+
+        print("\nSearch player:\n")
+        option_choice = input("All players [1]"
+                              + "    By tournament [2]    "
+                              + "    Change player ranking [3]\n\n")
+        while str(option_choice) not in "123":
+            print("Choose a number between 1 and 3")
+            option_choice = input("All players [1]"
+                                  + "    By tournament [2]    "
+                                  + "    Change Player Ranking [3]\n\n")
+            continue
+        return option_choice
+
+    @staticmethod
+    def print_tournament_search_options():
+
+        """A static method to print the search options for a tournament."""
+
+        print("\nSee tournament:\n")
+        option_choice = input("All Tournaments [1]    "
+                              + "See Tournament Details [2]\n\n")
+        while str(option_choice) not in "123":
+            print("Choose a number between 1 and 2")
+            option_choice = input("All Tournaments [1]     See Tournament"
+                                  + " Details [2]\n\n")
+            continue
+        return option_choice
+
+    @staticmethod
     def quit_current_page():
-        quit_current_page = input("Return to Main Page ? [Y/N]\n")
+
+        """A static method to propose to quit the current page."""
+
+        quit_current_page = input("\nReturn to Main Page ? [Y/N]\n")
         while quit_current_page not in "yYnN":
             quit_current_page = input("Return to Main Page ? [Y/N]\n")
             continue
         return quit_current_page
 
     @staticmethod
-    def print_first_option():
-        start_tournament = pyfiglet.figlet_format("NEW TOURNAMENT",
-                                                  font="digital")
-        print(start_tournament)
+    def search_tournament():
+
+        """A static method to filter the search for a tournament."""
+
+        choice = input("\nSearch tournament (Enter option number): \n\n"
+                       + "By name [A]\n"
+                       + "By location [B]\n"
+                       + "By year [C]\n\n")
+
+        while choice not in "aAbBcC":
+            print("Choose a correct number.")
+            choice = input("\nSearch tournament (Enter option number): \n\n"
+                           + "By name [A]\n"
+                           + "By location [B]\n"
+                           + "By year [C]\n\n")
+            continue
+
+        return choice
+
+    @staticmethod
+    def choose_ranking_or_name():
+
+        """A static method to propose to sort players by ranking
+        or by last name."""
+
+        print("\nSort players:\n")
+        option_choice = input("By ranking [A]"
+                              + "    By last name [B]\n\n")
+        while str(option_choice) not in "aAbB":
+            print("Choose a letter between A and B")
+            option_choice = input("By ranking [A]"
+                                  + "    By last name [B]\n\n")
+            continue
+        return option_choice
 
     @staticmethod
     def quit_program():
+
+        """A static method to print the end of the program."""
+
         print("\nThank you for using Chess Scores Manager !")
