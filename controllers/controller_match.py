39f5994afb23c32e157ddbf88b7b734328464f7f
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-03-02 17:20:46 +0200

    PEP8 + docstrings completed

--- controllers/controller_match.py
+++ controllers/controller_match.py
@@ -1,33 +1,36 @@
-import os
-import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.abspath(os.path.dirname(current_dir))
-sys.path.append(root_dir)
-
 from models.model_match import ModelMatch
-from models.model_player import ModelPlayer
 from views.view_match import ViewMatch
-from controllers.controller_player import ControllerPlayer
 
 
 class ControllerMatch:
+
+    """A Controller for Match Class.
+
+    The controller can perform the following actions :
+
+    - Start a match ;
+    - Print the winner and the score of a match."""
 
     def __init__(self, model, view):
         self.model = ModelMatch
         self.view = ViewMatch
 
     def start_match(first_player, second_player):
-        first_player_name = first_player.surname
-        second_player_name = second_player.surname
+
+        """A function to start a match."""
+
+        first_player_name = first_player.last_name
+        second_player_name = second_player.last_name
         return ViewMatch.start_match(first_player_name, second_player_name)
 
     def print_winner_and_score(first_player, second_player):
 
+        """A function to print the winner and the score of a match."""
+
         match = ModelMatch("", "", "", "")
 
-        f_p = first_player.surname
-        s_p = second_player.surname
+        f_p = first_player.last_name
+        s_p = second_player.last_name
 
         result = ViewMatch.get_match_result(f_p)
 
