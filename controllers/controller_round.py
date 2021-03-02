7f3d1ebfeeb28708cebbd4557615d1408a324e34
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-03-02 17:22:58 +0200

    PEP8 + docstrings completed

--- controllers/controller_round.py
+++ controllers/controller_round.py
@@ -1,34 +1,40 @@
-from models.model_match import ModelMatch
-from models.model_player import ModelPlayer
 from models.model_round import ModelRound
 from views.view_round import ViewRound
-from controllers.controller_player import ControllerPlayer
-from controllers.controller_match import ControllerMatch
 from datetime import datetime
-
-import os
-import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.abspath(os.path.dirname(current_dir))
-sys.path.append(root_dir)
 
 
 class ControllerRound:
+
+    """A Controller for Round Class.
+
+    The controller can perform the following actions :
+
+    - Start a round ;
+    - End a round ;
+    - Print the results of a round."""
 
     def __init__(self, model, view):
         self.model = ModelRound
         self.view = ViewRound
 
     def start_round(i):
+
+        """A function to start a round."""
+
         ViewRound.start_round(i)
         start_date = datetime.now().replace(microsecond=0)
         return start_date
 
     def end_round():
+
+        """A function to end a round."""
+
         end_date = datetime.now().replace(microsecond=0)
         return end_date
 
     def print_round_results(i, start_date, end_date, list_of_matches):
+
+        """A function to print the results of a round."""
+
         return ViewRound.print_round_results(i, start_date, end_date,
                                              list_of_matches)
